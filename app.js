a0659c88a55bffb0d8790a82befb394d8f9cbff6
SSWren <sydney.wrenwick@gmail.com>
2022-08-06 13:26:10 -0400

    Update without rollDice

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 class Casino {
-    // Write code here
+    constructor (name, isFakeCoin) {
+        this.timesPlayed = 0;
+        this.name = name;
+        this.isFakeCoin = isFakeCoin;
+    }
+
+    playGame(betAmount) {
+        if (Math.random() >= 0.5 || this.isFakeCoin) {
+            console.log(this.name + " WINS!");
+        } else {
+          console.log("Player WINS " + betAmount + " dollars!");
+        }
+        this.timesPlayed +=1
+    }
+
+    rollDie()
 };
 
 // TESTS
+/*
 const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
 myCasino.playGame(25);
 myCasino.playGame(35);
+*/
 
 // BONUS TESTS
-/*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
